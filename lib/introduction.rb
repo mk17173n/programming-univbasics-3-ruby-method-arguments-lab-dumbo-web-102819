c8a787677d52c02a380621d09ecbd7d966993ac7
Monsur Khan <monsur.khan@gmail.com>
2019-10-22 02:48:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,19 +6,16 @@
 def introduction(name)
 puts "Hi, my name is #{name}."
 end
-introduction(Josh)
+
 
 def introduction_with_language(name, language);
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language (Josh, React)
-
 def introduction_with_language_optional(name, language ="Ruby")
 puts "Hi, my name is # {name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional(Edwin, Python)
 
 
 
